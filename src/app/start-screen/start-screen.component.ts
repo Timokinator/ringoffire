2fb7b3cdbbaaeab4db81d7b4598465ea67fe43a9
Timo Kempf <124527843+Timokinator@users.noreply.github.com>
2023-10-17 22:03:24 +0200

    card-stack added

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./start-screen.component.scss']
 })
 export class StartScreenComponent {
+
+  constructor(private router: Router) { }
+
+newGame() {
+  //Start Game
+
+
+
+
+
+  this.router.navigateByUrl('/game');
+
+
+}
+
+
+
+
+
+
+
 
 }
